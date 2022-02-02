f0499444900a959c65929cdf2f2313f0e45eda14
Kovacs Botond <kovacs.botond1@telekom.hu>
2022-02-02 18:58:16 +0100

    wip

--- src/reservation/forms.py
+++ src/reservation/forms.py
@@ -2,7 +2,24 @@ from dataclasses import field
 from django import forms
 from .models import Reservation
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+    input_type = 'time'
+
 class ReserveTableForm(forms.ModelForm):
+    
+
     class Meta:
         model = Reservation
         fields = '__all__'
+        widgets = {
+            'date': DateInput(),
+            'time': TimeInput()
+        }
+
+
+
+
+
